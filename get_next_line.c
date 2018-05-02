cf4756bb6f1a316d993a011b5e9a0746680b2863
Sacha REQUIEM <srequiem@e2r2p13.42.fr>
2018-05-02 22:55:35 +0200

    Maybe

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: srequiem <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/20 18:23:31 by srequiem          #+#    #+#             */
-/*   Updated: 2018/05/02 19:12:04 by srequiem         ###   ########.fr       */
+/*   Updated: 2018/05/02 22:47:41 by srequiem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -49,30 +49,40 @@ int			riding_zone(int fd, char **str)
 	return (0);
 }
 
+char		*ft_strdupn(char *str, int n)
+{
+	char	*res;
+	int		i;
+
+	i = 0;
+	if (!(res = (char*)malloc((n + 1) * sizeof(char))))
+		return (0);
+	while (i < n)
+	{
+		res[i] = str[i];
+		i++;
+	}
+	res[i] = '\0';
+	return (res);
+}
+
 int			get_next_line(const int fd, char **line)
 {
 	static char	*str;
 	int			i;
+	char		*tmp;
 
 	i = 0;
+	tmp = NULL;
 	if ((is_valid(fd, line, &str) == -1) || (riding_zone(fd, &str) == -1))
 		return (-1);
 	if (str[i])
 	{
 		while (str[i] != '\n' && str[i])
 			i++;
-		if (i == 0) // Si premier char == \n
-		{
-			(*line) = ft_strdup("");
-			//str = &str[i + 1];
-			ft_strcpy(str, &str[i + 1]);
-		}
-		else
-		{
-			(*line) = ft_strsub(str, 0, i);
-			//str = &str[i + 1];
-			ft_strcpy(str, &str[i + 1]);
-		}
+		(*line) = ft_strdupn(str, i);		//(*line) = ft_strsub(str, 0, i);
+		tmp = &str[i + 1];					//str = &str[i + 1];
+		ft_strcpy(str, tmp);				//ft_strcpy(str, &str[i + 1]);
 		return (1);
 	}
 	else
@@ -84,6 +94,7 @@ int		main(int argc, char **argv)
 {
 	int		fd;
 	char		*line;
+	int	ret;
 
 	if (argc != 2)
 	{
@@ -92,13 +103,16 @@ int		main(int argc, char **argv)
 	}
 	if ((fd = open(argv[1], O_RDONLY)) < 0)
 		return (0);
-	while (get_next_line(fd, &line))
+	while ((ret = get_next_line(fd, &line)) > 0)
 	{
-		printf("%s\n", line);
+		printf("[%d]:%s\n", ret, line);
 		ft_strdel(&line);
 	}
+	printf("[%d]:%s\n", ret, line);
+	close (fd);
 	ft_strdel(&line);
-	while (1)
-		;
+	//while (1)
+	//{
+	//}
 	return (0);
 }*/
