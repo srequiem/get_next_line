dfcbf4e1561cef7e15a869974b5de7a0c04137d5
Sacha REQUIEM <srequiem@e2r3p12.42.fr>
2018-05-27 18:13:52 +0200

    PsshPsssh

--- get_next_line.c
+++ get_next_line.c
@@ -6,90 +6,89 @@
 /*   By: srequiem <srequiem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/20 18:23:31 by srequiem          #+#    #+#             */
-/*   Updated: 2018/05/10 17:49:36 by srequiem         ###   ########.fr       */
+/*   Updated: 2018/05/27 17:57:46 by srequiem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include <stdio.h>
-#include <limits.h>
 
-int			is_valid(int fd, char **line, char **str)
+int		riding_zone(char **line, char **str)
 {
-	if (fd < 0 || line == NULL)
-		return (-1);
-	if (!*str)
+	char	*tmp;
+	int		n;
+
+	n = 0;
+	if (*str && (n = ft_is_in(*str, '\n')) != -1)
 	{
-		if (!(*str = (char*)ft_memalloc((BUFF_SIZE + 1) * sizeof(char))))
-			return (-1);
+		*line = ft_strdupn(*str, n);
+		tmp = ft_strdup(*str + n + 1);
+		free(*str);
+		*str = tmp;
+		return (1);
 	}
 	return (0);
 }
 
-int			riding_zone(int fd, char **str)
+int		ft_read(int fd, char **str, char **line)
 {
-	int		bytes;
 	char	buff[BUFF_SIZE + 1];
 	char	*tmp;
+	int		bytes;
 
-	if (BUFF_SIZE <= 0)
-		return (-1);
-	if (**str)
-		return (0);
 	while ((bytes = read(fd, buff, BUFF_SIZE)) > 0)
 	{
 		buff[bytes] = '\0';
-		tmp = ft_strjoin(*str, buff);
-		ft_strdel(str);
-		*str = tmp;
+		if (*str)
+		{
+			tmp = *str;
+			*str = ft_strjoin(*str, buff);
+			free(tmp);
+		}
+		else
+			*str = ft_strdup(buff);
+		if (riding_zone(line, str))
+			return (1);
 	}
-	return (bytes == -1 ? -1 : 0);
-}
-
-int			get_next_line(const int fd, char **line)
-{
-	static char	*str[OPEN_MAX];
-	int			i;
-	char		*tmp;
-
-	i = 0;
-	tmp = NULL;
-	if ((is_valid(fd, line, &str[fd]) == -1) ||
-		(riding_zone(fd, &str[fd]) == -1))
-		return (-1);
-	if (str[fd][i])
+	if (*str && bytes == 0 && *str[0] != '\0')
 	{
-		while (str[fd][i] != '\n' && str[fd][i])
-			i++;
-		*line = ft_strdupn(str[fd], i);
-		tmp = str[fd];
-		str[fd] = ft_strdup(str[fd] + i + 1);
-		ft_strdel(&tmp);
+		*line = ft_strdup(*str);
+		*str = NULL;
 		return (1);
 	}
-	else
-	{
-		*line = ft_strdup(str[fd]);
-		ft_strdel(&str[fd]);
-	}
-	return (0);
+	return ((bytes == -1) ? -1 : 0);
 }
 
+int		get_next_line(const int fd, char **line)
+{
+	static char	*str[OPEN_MAX];
+
+	if (fd < 0 || line == NULL || BUFF_SIZE < 1)
+		return (-1);
+	if (str[fd])
+	{
+		if (riding_zone(line, &str[fd]))
+			return (1);
+		else
+			return (ft_read(fd, &str[fd], line));
+	}
+	else
+		return (ft_read(fd, &str[fd], line));
+	return (0);
+}
+/*
+#include <stdio.h>
 int		main(int argc, char **argv)
 {
-   int		fd;
-   int		fd2;
-   char		*line = NULL;
+	char	*line = NULL;
+	int		fd;
 	int		ret;
 
-	if (argc < 0)
+	if (argc < 2)
 	{
 		ft_putstr("A Few Moments later...\n");
 		return (0);
 	}
 	if ((fd = open(argv[1], O_RDONLY)) < 0)
-		return (0);
-	if ((fd2 = open(argv[2], O_RDONLY)) < 0)
 		return (0);
 	while ((ret = get_next_line(fd, &line)) > 0)
 	{
@@ -101,18 +100,5 @@ int		main(int argc, char **argv)
 	printf("[%d]:%s\n", ret, line);
 	ft_strdel(&line);
 	close(fd);
-
-	while ((ret = get_next_line(fd2, &line)) > 0)
-	{
-		printf("[%d]:%s\n", ret, line);
-		ft_strdel(&line);
-	}
-	ft_strdel(&line);
-	ret = get_next_line(fd2, &line);
-	printf("[%d]:%s\n", ret, line);
-	ft_strdel(&line);
-	close(fd2);
-	//while (1)
-	//{}
 	return (0);
-}
+}*/
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: srequiem <srequiem@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/20 18:12:27 by srequiem          #+#    #+#             */
-/*   Updated: 2018/05/10 17:31:32 by srequiem         ###   ########.fr       */
+/*   Updated: 2018/05/27 17:53:50 by srequiem         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,9 @@
 
 # include "libft/libft.h"
 # include <fcntl.h>
+# include <limits.h>
 # define ERR_READ "open failed for some reasons"
-# define BUFF_SIZE 7
+# define BUFF_SIZE 9
 
 int		get_next_line(const int fd, char **line);
 
